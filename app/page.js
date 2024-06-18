b075394e40f6f2f655852a258f5c2fc3b08ec5f5
apktntaj <maiaelarsya@gmail.com>
2024-06-18 11:09:13 +0700

    refactor: Update page title and subtitle for better user understanding

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 import Hero from "./ui/Hero";
 
-const title = "Enhance Your Business Efficiency with Us";
+const title = "Selasaikan dokumen pabeanmu dalam menit.";
 const subtitle =
-  "Experience the future of business operations with our state-of-the-art automation and CRM solutions. Our platform is designed to streamline your workflows, boost productivity, and foster stronger customer relationships. From automating repetitive tasks to providing deep insights into customer interactions, our tools empower your team to work smarter, not harder. Unlock unprecedented efficiency and elevate your business to new heights with our innovative technology.";
+  "Malika akan membantu anda menyelesaikan problem pabean kamu dengan cepat. Hidup terlalu singkat untuk mengerjakana pekerjaan repetitif. Biar kami yang melakukannya.";
 
 export default function Home() {
   return (
--- app/ui/Hero.jsx
+++ app/ui/Hero.jsx
@@ -2,22 +2,22 @@ import Image from "next/image";
 
 const Hero = ({ title, subtitle }) => {
   return (
-    <div className="px-10 lg:-mt-10 hero min-h-screen flex justify-evenly items-center">
-      <div className="hero-content text-center lg:text-left">
+    <div className="px-10 lg:-mt-[110px] hero min-h-screen flex justify-evenly items-center">
+      <div className="flex flex-col hero-content text-center lg:text-left">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">{title}</h1>
+          <h1 className="text-4xl font-bold">{title}</h1>
           <p className="py-6">{subtitle}</p>
-          <div className="flex gap-3 justify-center">
-            <button className="btn btn-accent">Sign Up</button>
-            <button className="btn btn-success">Login</button>
-          </div>
+        </div>
+        <div className="flex gap-3 justify-start w-full">
+          <button className="btn btn-accent">Sign Up</button>
+          <button className="btn btn-success">Login</button>
         </div>
       </div>
       <div>
         <Image
-          src={"vercel.svg"}
-          width={500}
-          height={500}
+          src={"/logo.png"}
+          width={400}
+          height={400}
           className="hidden lg:block -mt-16"
         />
       </div>
