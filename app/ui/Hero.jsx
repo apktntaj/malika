41fd477b4c5888dd69829ddbeb68f8d9cfe0a831
apktntaj <maiaelarsya@gmail.com>
2024-08-07 12:12:55 +0700

    delete button in home page

--- app/ui/Hero.jsx
+++ app/ui/Hero.jsx
@@ -3,29 +3,20 @@ import Link from "next/link";
 
 const Hero = ({ title, subtitle }) => {
   return (
-    <div className="px-10 lg:-mt-[110px] hero min-h-screen flex justify-evenly items-center">
+    <div className="px-2 lg:-mt-[110px] hero min-h-screen flex justify-around items-center">
       <div className="flex flex-col hero-content text-center lg:text-left">
         <div className="max-w-md">
           <h1 className="text-4xl font-bold">{title}</h1>
           <p className="py-6">{subtitle}</p>
         </div>
-        <div className="flex gap-3 justify-start w-full">
-          <button className="btn btn-accent">
-            <Link href={"/daftar"}>Daftar</Link>
-          </button>
-          <button className="btn btn-success">
-            <Link href={"/dashboard"}>Masuk</Link>
-          </button>
-        </div>
       </div>
-      <div>
-        <Image
-          src={"/logo.png"}
-          width={400}
-          height={400}
-          className="hidden lg:block -mt-16"
-        />
-      </div>
+
+      <Image
+        src={"/logo.png"}
+        width={450}
+        height={450}
+        className="hidden lg:block -mt-16"
+      />
     </div>
   );
 };
